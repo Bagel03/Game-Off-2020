b22273c5f499ce9262909292974a13a755d6d5df
Jason E. Smith <jaysmith024@gmail.com>
2020-11-03 06:32:29 -0500

    update

--- src/game/Main/index.js
+++ src/game/Main/index.js
@@ -1,14 +1,27 @@
 
 @tag("game-off");
 namespace `game` (
-    class Main extends Application {
+    class Main extends World {
         constructor(element){
             super(element);
         }
 
         async onConnected() {
             await super.onConnected();
-            console.log("Game-Off 2020!")
+            console.log("Game-Off 2020!");
+        }
+
+        onFixedUpdate = (time) => {
+
+        }
+
+        onDraw = (interpolation) => {
+            
+        }
+
+
+        onUpdate = (timestamp, delta) => {
+            
         }
     }
 );
