034b3165b47359ea1595a293746007c5a878337f
Edward Badel <edwardcbadel@gmail.com>
2020-11-08 15:06:19 -0500

    Added vectors and rectangles, worked o n camera

---
+++ src/game/Modules/utils/Rectangle.js
@@ -0,0 +1,27 @@
+import { Vector } from "./Vector";
+
+export class Rectangle{
+    /**
+     * 
+     * @param {Vector} position The position of the top left coner of the rectangle 
+     * @param {Vector} size The width and height of the rectangle
+     */
+    constructor(position, size){
+        this.position = position;
+        this.size = size;
+    }
+
+    get top(){return this.position.y;}
+    get left(){return this.position.x;}
+    get right(){return this.position.x + this.size.x;}
+    get bottom(){return this.position.y + this.size.y;}
+
+    get width(){return this.size.x}
+    get height(){return this.size.y}
+
+    /**
+     * 
+     * @param {Vector} vector 
+     */
+    setCenter = vector => this.position.setFromVector(vector.sub(this.size.copy().div(2)));
+}
