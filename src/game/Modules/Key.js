057f2f12851d70d47471da61db5d0b7a01c5ec00
Edward Badel <edwardcbadel@gmail.com>
2020-11-08 17:39:24 -0500

    potential bugfix

--- src/game/Modules/Key.js
+++ src/game/Modules/Key.js
@@ -1,27 +1,27 @@
 namespace `game.modules` (
 
-window.Key = {
-  _pressed: {},
-  LEFT: "ArrowLeft",
-  UP: "ArrowUp",
-  RIGHT: "ArrowRight",
-  DOWN: "ArrowDown",
-  A: "KeyA",
-  D: "KeyD",
+  window.Key = {
+    _pressed: {},
+    LEFT: "ArrowLeft",
+    UP: "ArrowUp",
+    RIGHT: "ArrowRight",
+    DOWN: "ArrowDown",
+    A: "KeyA",
+    D: "KeyD",
 
-  isDown: function(code) {
-    return this._pressed[code];
+    isDown: function(code) {
+      return this._pressed[code];
+    },
+    
+    onKeydown: function(e) {
+      this._pressed[e.code] = true;
+    },
+    
+    onKeyup: function(e) {
+      delete this._pressed[e.code];
+    }
   },
-  
-  onKeydown: function(e) {
-    this._pressed[e.code] = true;
-  },
-  
-  onKeyup: function(e) {
-    delete this._pressed[e.code];
-  }
-};
-window.addEventListener('keyup', function(e) { Key.onKeyup(e); }, false);
-window.addEventListener('keydown', function(e) { Key.onKeydown(e); }, false);
+  window.addEventListener('keyup', function(e) { Key.onKeyup(e); }, false),
+  window.addEventListener('keydown', function(e) { Key.onKeydown(e); }, false)
 
 )
