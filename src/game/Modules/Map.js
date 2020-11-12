f98b80a4f907535af371f637dc98c609c49ed17c
Edward Badel <edwardcbadel@gmail.com>
2020-11-12 14:01:21 -0500

    added map class

--- src/game/Modules/Map.js
+++ src/game/Modules/Map.js
@@ -1,1 +1,15 @@
-export class Map{}
+namespace `game.modules`(
+    class Map{
+        constructor(JSONobj, options){
+            this.map = loader.load(JSONobj) //<- Nashorns loader goes there
+            this.spritesheet = options.spritesheet;
+        }
+
+        render(context){
+            this.map.layers.forEach(layer => {
+                //do something for now
+            });
+        }
+    }
+    
+)
