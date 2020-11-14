070d2647dacfe9f199a41eb33e04900cd262c468
Edward Badel <edwardcbadel@gmail.com>
2020-11-14 14:47:59 -0500

    map renderer using canvas

--- src/experiments/tiled/renderers/canvas/Topdown.js
+++ src/experiments/tiled/renderers/canvas/Topdown.js
@@ -1,10 +1,8 @@
 namespace `experiments.tiled.renderers.canvas` (
     class Topdown {
-        constructor(context, map, camera){
-            this.context=context;
-            this.ctx=context;
+        constructor(map){
+            this.context=document.createElement('canvas').getContext('2d');
             this.map=map;
-            this.camera = camera;
             this.tileAtlas = new Image();
             this.tileAtlas.src="resources/maps/topdown/atlas_16x.png";
 
@@ -17,36 +15,54 @@ namespace `experiments.tiled.renderers.canvas` (
         }
 
         drawLayer (layer) {
-            var map=this.map;
-            var startCol = Math.floor(this.camera.x / map.tilewidth);
-            var endCol = startCol + (this.camera.width / map.tilewidth);
-            var startRow = Math.floor(this.camera.y / map.tileheight);
-            var endRow = startRow + (this.camera.height / map.tileheight);
-            var offsetX = -this.camera.x + startCol * map.tilewidth;
-            var offsetY = -this.camera.y + startRow * map.tileheight;
-
-            for (var c = startCol; c <= endCol; c++) {
-                for (var r = startRow; r <= endRow; r++) {
-                    // debugger;
-                    var tile = map.getTile(layer, c, r);
-                    var x = (c - startCol) * map.tilewidth + offsetX;
-                    var y = (r - startRow) * map.tileheight + offsetY;
-                    debugger;
-                    // if (tile !== 0) { // 0 => empty tile
-                        this.context.drawImage(
-                            this.tileAtlas, // image
-                            c * map.tilewidth, // source x
-                            r * map.tileheight, // source y
-                            map.tilewidth, // source width
-                            map.tileheight, // source height
-                            Math.round(x),  // target x
-                            Math.round(y), // target y
-                            map.tilewidth, // target width
-                            map.tileheight // target height
-                        );
-                    // }
+            const {map} = this;
+            for(let row = 0; row < map.width; row++){
+                for(let col = 0; col < map.width; col++){
+                    const tile = map.getTile(layer, col, row);
+                    if(tile === 0) continue;
+                    this.context.drawImage(
+                        this.tileAtlas, 
+                        tile.x, 
+                        tile.y, 
+                        map.tilewidth,
+                        map.tileheight,
+                        col * map.tilewidth,
+                        row * map.tileheight,
+                        map.tilewidth,
+                        map.tileheight
+                    )
                 }
             }
+            // var map=this.map;
+            // var startCol = Math.floor(this.camera.x / map.tilewidth);
+            // var endCol = startCol + (this.camera.width / map.tilewidth);
+            // var startRow = Math.floor(this.camera.y / map.tileheight);
+            // var endRow = startRow + (this.camera.height / map.tileheight);
+            // var offsetX = -this.camera.x + startCol * map.tilewidth;
+            // var offsetY = -this.camera.y + startRow * map.tileheight;
+
+            // for (var c = startCol; c <= endCol; c++) {
+            //     for (var r = startRow; r <= endRow; r++) {
+            //         // debugger;
+            //         var tile = map.getTile(layer, c, r);
+            //         var x = (c - startCol) * map.tilewidth + offsetX;
+            //         var y = (r - startRow) * map.tileheight + offsetY;
+            //         debugger;
+            //         // if (tile !== 0) { // 0 => empty tile
+            //             this.context.drawImage(
+            //                 this.tileAtlas, // image
+            //                 c * map.tilewidth, // source x
+            //                 r * map.tileheight, // source y
+            //                 map.tilewidth, // source width
+            //                 map.tileheight, // source height
+            //                 Math.round(x),  // target x
+            //                 Math.round(y), // target y
+            //                 map.tilewidth, // target width
+            //                 map.tileheight // target height
+            //             );
+            //         // }
+            //     }
+            // }
         }
 
 
@@ -58,18 +74,18 @@ namespace `experiments.tiled.renderers.canvas` (
             for (var r = 0; r < map.height; r++) {
                 x = - this.camera.x;
                 y = r * map.tileheight - this.camera.y;
-                this.ctx.beginPath();
-                this.ctx.moveTo(x, y);
-                this.ctx.lineTo(width, y);
-                this.ctx.stroke();
+                this.context.beginPath();
+                this.context.moveTo(x, y);
+                this.context.lineTo(width, y);
+                this.context.stroke();
             }
             for (var c = 0; c < map.width; c++) {
                 x = c * map.tilewidth - this.camera.x;
                 y = - this.camera.y;
-                this.ctx.beginPath();
-                this.ctx.moveTo(x, y);
-                this.ctx.lineTo(x, height);
-                this.ctx.stroke();
+                this.context.beginPath();
+                this.context.moveTo(x, y);
+                this.context.lineTo(x, height);
+                this.context.stroke();
             }
         }
     }
