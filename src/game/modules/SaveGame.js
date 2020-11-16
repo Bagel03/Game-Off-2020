d6c956df86d9a59a9eae9af1705432bf8c798b67
aldehyt <aldehyt@gmail.com>
2020-11-16 15:49:54 +0100

    savegame prototype

--- src/game/modules/SaveGame.js
+++ src/game/modules/SaveGame.js
@@ -2,15 +2,23 @@
 namespace`game.modules`(
     class SaveGame{
         constructor(){
-            this.listenSonicData()
             this.eventArray = [];
+            this.newStorage = localStorage
+            this.listenSonicData()
+            
         }
         listenSonicData(){
             document.addEventListener("constructorData", (e)=>{
                 // console.log("Class SaveGame received event", e.detail)
                 this.eventArray.push(e.detail)
-                console.log(this.eventArray)
+                this.saveToLocalStorageAsJSON();
             })
+        }
+        saveToLocalStorageAsJSON(array =[]){
+            array = this.eventArray;
+            // jsondata = JSON.stringify(array[0])
+            console.log(array)
+            // this.newStorage["Save game"] = JSON.stringify(array)
         }
     }
 )
