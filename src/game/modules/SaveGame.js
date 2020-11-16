7170c6cef72c7763c1ed52c4cef882e4334727a1
aldehyt <aldehyt@gmail.com>
2020-11-16 15:16:05 +0100

    pushing data into an array

--- src/game/modules/SaveGame.js
+++ src/game/modules/SaveGame.js
@@ -3,10 +3,13 @@ namespace`game.modules`(
     class SaveGame{
         constructor(){
             this.listenSonicData()
+            this.eventArray = [];
         }
         listenSonicData(){
             document.addEventListener("constructorData", (e)=>{
-                console.log("Class SaveGame received event", e.detail)
+                // console.log("Class SaveGame received event", e.detail)
+                this.eventArray.push(e.detail)
+                console.log(this.eventArray)
             })
         }
     }
