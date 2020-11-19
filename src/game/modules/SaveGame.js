e1239ddba58a19d4cd1bb93ed434c5c2bb9a3952
aldehyt <aldehyt@gmail.com>
2020-11-19 10:21:21 +0100

    now we can save in localStorage

--- src/game/modules/SaveGame.js
+++ src/game/modules/SaveGame.js
@@ -1,27 +1,19 @@
 
 namespace`game.modules`(
     class SaveGame{
-        sonic = {
-            x: "",
-            y: "",
-        }    
-        world = {
-        
-            cameraX:"",
-            cameraY:"",
-        }
-
         constructor(){
-            this.eventArray = [];
-
-            // need a few loops and comparison
-            this.listenData("sonic", this.sonic)
-            this.listenData("world", this.world)
+            this.listeners = ["sonic","world"]
+            this.listenData(this.listeners);
         }
-        listenData(eventname, filter){
-            document.addEventListener(eventname, (e)=>{
-                console.log(typeof e.detail)
-            })
-        }
+       listenData(array=[]){
+           array.forEach(listener =>{
+               document.addEventListener(listener, even =>{
+                   for( const element in even.detail){
+                        console.log(element, even.detail[element])
+                        localStorage.setItem(`${listener}, ${element}`, even.detail[element])
+                   }
+               })
+           })
+       }
     }
 )
