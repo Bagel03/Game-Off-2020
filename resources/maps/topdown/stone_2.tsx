3e06235a30d464d452e4b9731e97caddcf8145ac
Jason E. Smith <jaysmith024@gmail.com>
2020-11-26 08:35:18 -0500

    added larger stone to map

---
+++ resources/maps/topdown/stone_2.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="stone_2" tilewidth="128" tileheight="128" tilecount="1" columns="1" objectalignment="topleft">
+ <editorsettings>
+  <export target="stone_2.json" format="json"/>
+ </editorsettings>
+ <image source="../../images/stone_2.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="19" y="55" width="93" height="40"/>
+  </objectgroup>
+ </tile>
+</tileset>
